fa355a3789942b9c5db74f834b6cc2160b51496a
Teerasej <teerasej@gmail.com>
2018-08-22 17:59:01 +0700

    Receive siri invoke

--- siri shortcut tryout/AppDelegate.swift
+++ siri shortcut tryout/AppDelegate.swift
@@ -40,6 +40,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool{
+
+        let viewController = window?.rootViewController as! ViewController
+        viewController.makeViewRed()
+        
+        return true;
+    
+    
+    }
 
 
 }
