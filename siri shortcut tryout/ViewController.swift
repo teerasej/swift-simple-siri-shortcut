c7645c2119e1a5f04d75467a54b3e2958533fe65
Teerasej <teerasej@gmail.com>
2018-08-22 17:58:44 +0700

    Register intent activity

--- siri shortcut tryout/ViewController.swift
+++ siri shortcut tryout/ViewController.swift
@@ -14,6 +14,23 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    public func makeViewRed() {
+        view.backgroundColor = .red
+    }
+    
+    @IBAction func buttonAction(sender: UIButton){
+        let activity = NSUserActivity(activityType: "th.in.nextflow.siri-shortcut-tryout.makeRed")
+        activity.title = "Make View Red"
+        activity.userInfo = ["color": "red"]
+        activity.isEligibleForSearch = true
+        activity.isEligibleForPrediction = true
+//        activity.persistentIdentifier = NSUserActivityPersistentIdentifie
+        view.userActivity = activity;
+        activity.becomeCurrent();
+        
+        makeViewRed();
+    }
 
 
 }
